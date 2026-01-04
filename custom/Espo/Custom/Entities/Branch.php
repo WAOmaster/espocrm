10adf9082ec641aebeeb8ac6d1f98575630fdaf4
WAOmaster <waoearth2013@gmail.com>
2026-01-04 19:31:53 +0530

    Add PHP Entity classes for Branch and LoanProduct to fix 404

---
+++ custom/Espo/Custom/Entities/Branch.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Espo\Custom\Entities;
+
+use Espo\Core\ORM\Entity;
+
+class Branch extends Entity
+{
+    public const ENTITY_TYPE = 'Branch';
+
+    protected $entityType = 'Branch';
+}
---
+++ custom/Espo/Custom/Entities/LoanProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Espo\Custom\Entities;
+
+use Espo\Core\ORM\Entity;
+
+class LoanProduct extends Entity
+{
+    public const ENTITY_TYPE = 'LoanProduct';
+
+    protected $entityType = 'LoanProduct';
+}
